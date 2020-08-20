21f1ffb6f7f4f560c5cd2f16dac03b2a563bf00a
Bongo <64612244+OMantid@users.noreply.github.com>
2020-08-20 01:49:20 -0700

    Updated and added some eventlisteners and logic for the calculator functions

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,18 +1,24 @@
+const num = document.getElementsByClassName('num');
+const opt = document.getElementsByClassName('operator');
+const input = document.querySelector('input');
+const equal = document.getElementById('equals')
+let numA, numB, operator;
+
 //Basic Math Operators
 function add(numA, numB){
-    return numA + numB;
+    input.value = Number(numA) + Number(numB);
 }
 
 function subtract(numA, numB){
-    return numA - numB;
+    input.value = Number(numA) - Number(numB);
 }
 
 function multiply(numA, numB){
-    return numA * numB;
+    input.value = Number(numA) * Number(numB);
 }
 
 function divide(numA, numB){
-    return numA / numB;
+    input.value = Number(numA) / Number(numB);
 }
 
 //Operate takes an operator and 2 numbers, then calls one of the above functions
@@ -25,7 +31,41 @@ function operate(operator, numA, numB){
         case '*': multiply(numA, numB);
         break;
         case '/': divide(numA, numB);
+        break;
         default:
             alert('Not an operator!');
     }
 }
+
+//Function that populates the display when clicking the number buttons
+
+[...num].forEach(e => e.addEventListener('click', function(){
+    input.value += e.textContent;
+}));
+
+[...opt].forEach(e => e.addEventListener('click', function(){
+    input.value += e.textContent;
+    numA = input.value.slice(0, 1);
+    operator = input.value.slice(-1);
+}));
+
+equal.addEventListener('click', function(){
+    numB = input.value.slice(-1);
+    operate(operator, numA, numB);
+});
+
+//Clears the display when the 'C' is pressed
+const clear = document.getElementById('clear');
+
+clear.addEventListener('click', function(){
+    input.value = '';
+});
+
+//Deletes last character when the '=>' is pressed
+const back = document.getElementById('backspace');
+
+back.addEventListener('click', function(){
+    let str = input.value;
+    let temp = str.slice(0, str.length-1);
+    input.value = temp;
+});
